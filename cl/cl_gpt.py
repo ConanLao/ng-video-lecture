95b66dc63a7d261bc3f11a908593311aa4abc976
clao13 <clao13@bloomberg.net>
2023-08-02 18:57:10 -0400

    Implemented the multihead attention but metrics don't match exactly.

--- cl/cl_gpt.py
+++ cl/cl_gpt.py
@@ -73,30 +73,27 @@ class Head(nn.Module):
         self.value = nn.Linear(n_emb, head_size, bias = False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
-    # def forward(self, x):
-    #     B, T, C = x.shape
-    #     k = self.key(x) # B, T, C
-    #     q = self.query(x) # B, T, C
-    #     wei = q @ k.transpose(-2, -1) * C ** -0.5
-    #     wei = wei.masked_fill(self.tril[ : T, : T] == 0, float('-inf'))
-    #     wei = F.softmax(wei, dim=-1)
-    #     v = self.value(x)
-    #     out = wei @ v
-    #     return out
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x) # B, T, C
+        q = self.query(x) # B, T, C
+        wei = q @ k.transpose(-2, -1) * C ** -0.5
+        wei = wei.masked_fill(self.tril[ : T, : T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_emb)
 
     def forward(self, x):
-        # input of size (batch, time-step, channels)
-        # output of size (batch, time-step, head size)
-        B,T,C = x.shape
-        k = self.key(x)   # (B,T,hs)
-        q = self.query(x) # (B,T,hs)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
-        # perform the weighted aggregation of the values
-        v = self.value(x) # (B,T,hs)
-        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
         return out
 
 class BigramLanguageModel(nn.Module):
@@ -104,7 +101,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_emb)
         self.position_embedding_table = nn.Embedding(block_size, n_emb)
-        self.sa_head = Head(n_emb)
+        # self.sa_head = Head(n_emb)
+        self.sa_head = MultiHeadAttention(4, n_emb // 4)
         self.lm_head = nn.Linear(n_emb, vocab_size)
     
     def forward(self, idx, targets=None):
